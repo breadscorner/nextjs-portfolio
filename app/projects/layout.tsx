7941a6e48a2c362495507b78e1fa676cfe5a4e67
breadscorner <hello@brettgill.io>
2024-04-01 22:56:05 -0700

    add suspense animation to projects

--- app/projects/layout.tsx
+++ app/projects/layout.tsx
@@ -1,9 +1,12 @@
+import React, { Suspense } from "react";
+import SuspenseLoader from "../components/loading";
+
 export default function ProjectsLayout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
 		<div className="relative min-h-screen bg-gradient-to-tl from-zinc-900 via-zinc-400/10 to-zinc-900 ">
-			{children}
+        <Suspense fallback={<SuspenseLoader />}>{children}</Suspense>
 		</div>
 	);
 }
