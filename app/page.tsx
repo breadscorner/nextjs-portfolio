3adb42626bc8cea8b40d288edbbfd11cbfa2f51e
breadscorner <hello@brettgill.io>
2024-04-25 20:07:53 -0700

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-md text-zinc-500 ">
-          Greater Vancouver Full Stack Developer
+          Full Stack Developer
         </h2>
       </div>
     </div>
